525541382c931f5a03c0434599964e0347e456d3
Gavin P <gavinjpeeler@gmail.com>
2025-10-14 20:07:58 -0700

    Switched to CommandXboxController

--- xrp-robot/src/main/java/frc/robot/RobotContainer.java
+++ xrp-robot/src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.xrp.XRPOnBoardIO;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -30,7 +31,7 @@ public class RobotContainer {
   private final XRPOnBoardIO m_onboardIO = new XRPOnBoardIO();
 
   // Assumes a gamepad plugged into channel 0
-  private final Joystick m_controller = new Joystick(0);
+  private final CommandXboxController m_controller = new CommandXboxController(0);
 
   // Create SmartDashboard chooser for autonomous routines
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
